f06b078e4f6088b43747aeb69e12f03ddab674f8
Kevin King <king.p.kevin@gmail.com>
2016-02-20 14:57:03 -0800

    Expanded backbone example

--- example.js
+++ example.js
@@ -17,8 +17,17 @@ FactoryBro.define('user', Sidebar, class UserFactory extends FactoryDefinition {
   static valid(instance) {
     instance.set('valid', true)
   }
+
+  static has_mobile(instance, options={}) {
+    var cc = "+1"
+    if( options.country_code ) {
+      cc = options.country_code
+    }
+    instance.set('mobile', cc+"numbers")
+  }
 });
 console.log(FactoryBro.build('user'))
 console.log(FactoryBro.build('user', ['valid']))
+console.log(FactoryBro.build('user', ['valid', 'has_mobile'], {country_code:'42'}))
 
 // console.log(Sidebar);
